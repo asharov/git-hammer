130953066a35eefc621ebd8e6fec4e7a33f95fac
Jaakko Kangasharju <ashar@iki.fi>
2019-12-18 17:21:06 +0100

    Fix date in latest test repository commit

--- tests/hammer_test.py
+++ tests/hammer_test.py
@@ -9,7 +9,7 @@ class HammerTest(unittest.TestCase):
 
     _main_repo_initial_commit_hexsha = 'c153f2881f0f0025a9ff5754e74111333ce859cd'
     _main_repo_second_commit_hexsha = '5151985f7e3551c73ccb65cda2b021194b30b30a'
-    _main_repo_head_commit_hexsha = '9f5ed1b1c43e211a30e6b9cc60ce26540ab9df70'
+    _main_repo_head_commit_hexsha = '2a57b201bbdd9345842b8b9b5f75789b3452353e'
 
     def _fetch_commit(self, hexsha, hammer=None):
         if hammer is None:
